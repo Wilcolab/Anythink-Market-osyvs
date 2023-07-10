a7b3179a9f370e257b3eaec886cadccd6e0a8334
Tidomann <tidomann@gmail.com>
2023-07-10 16:25:36 +0000

    faker dependancy removed

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -1,63 +1,53 @@
-import random
-import sqlalchemy
 
-from faker import Faker
-from app.core.config import get_app_settings
+
+from sqlalchemy import create_engine
 from sqlalchemy.sql import text
-from app.models.domain.users import UserInDB
-from app.models.domain.items import Item
-from app.models.domain.comments import Comment
 
-fake = Faker()
+import random
+import string
+import os
 
-SETTINGS = get_app_settings()
-DATABASE_URL = SETTINGS.database_url
+#find database connection URL
+DATABASE_URL = os.environ['DATABASE_URL'].replace("postgres://", "postgresql://")
 
-engine = sqlalchemy.create_engine(DATABASE_URL)
+engine = create_engine(database_url, echo=False)
 
-with engine.connect as connection:
-    for i in range(100):
-        test_username = "Test_User_{i}"
-        test_email = fake.email()
-        test_password = fake.password()
-        test_bio = fake.paragraph()
-        test_image = fake.image_url()
-        test_user = UserInDB(username=test_username, email=test_email, bio=test_bio, image=test_image)
+user_insert_statement = text("""INSERT INTO users(username, email, salt, bio, hashed_password, is_verified) VALUES(:username, :email, :salt, :bio, :hashed_password, :is_verified)""")
+select_last_user_id = text("""SELECT * FROM users ORDER BY id DESC LIMIT 1""")
+item_statement = text("""INSERT INTO items(slug, title, description, seller_id) VALUES(:slug, :title, :description, :seller_id)""")
 
-        connection.execute(
-            text(f"""
-            INSERT INTO users (username, salt, email, bio, image)
-            VALUES ('{test_user.username}', '', '{test_user.email}', '{test_user.bio}', '{test_user.image}')
-            """)
-        )
+letters = string.ascii_lowercase
 
-        HASHED_PASSWORD = 'test12345'
-        connection.execute(
-            text(f"UPDATE user SET salt = 'test', hashed_password = '{HASHED_PASSWORD}'")
-        )
+def create_user_and_item(con, slug, is_verified):
+    username_length = random.randint(10,15)
+    random_username = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(username_length))
 
-        test_slug = fake.slug()
-        test_title = fake.sentance()
-        test_description = fake.description()
-        test_tags = [fake.word() for _ in range(3)]
-        test_item_image = fake.image_url()
-        test_body = fake.text()
-        test_item = Item(slug=test_slug, description=test_description, tags=test_tags, seller=test_user,
-                         favorited=fake.boolean(), favorites_count=random.randint(0, 100), image=test_item_image,
-                         body=test_body)
-        connection.execute(
-            text(f"""
-            INSERT INTO items (slug, title, description, seller_id, image, body)
-            VALUES ('{test_item.slug}', '{test_item.title}', '{test_item.description}',
-            {i}, '{test_item.image}', '{test_item.body}') 
-            """)
-        )
+    user = {
+        'username': random_username,
+        'email': f'{random_username}@mail.com',
+        'salt': 'abc',
+        'bio': 'bio',
+        'hashed_password': '12345689',
+        'is_verified': is_verified
+    }
 
-        test_comment_body = fake.text()
-        test_comment = Comment(body=test_comment_body, seller=test_user)
-        connection.execute(
-            text(f"""
-                    INSERT INTO comments (body, seller_id, item_id) 
-                    VALUES ('{test_comment.body}', {i}, {i})
-                    """)
-        )
+    con.execute(user_insert_statement, **user)
+
+    result = con.execute(select_last_user_id)
+
+    generated_user_id = None
+    for row in result:
+        generated_user_id = row['id']
+
+    item = {
+        'slug': slug,
+        'title': 'title',
+        'description': ' description',
+        'seller_id': generated_user_id
+     }
+
+    con.execute(item_statement, **item)
+
+with engine.connect() as con:
+    create_user_and_item(con, 'verified_seller_item', True)
+    create_user_and_item(con, 'not_verified_seller_item', False)
