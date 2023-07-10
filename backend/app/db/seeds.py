8889a154ff47dd23b727b7b47c6ca442a422a96b
Tidomann <tidomann@gmail.com>
2023-07-10 17:02:41 +0000

    Added comments to dataseeding

--- backend/app/db/seeds.py
+++ backend/app/db/seeds.py
@@ -1,5 +1,3 @@
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
@@ -14,7 +12,14 @@ engine = create_engine(DATABASE_URL, echo=False)
 
 user_insert_statement = text("""INSERT INTO users(username, email, salt, bio, hashed_password) VALUES(:username, :email, :salt, :bio, :hashed_password)""")
 select_last_user_id = text("""SELECT * FROM users ORDER BY id DESC LIMIT 1""")
-item_statement = text("""INSERT INTO items(slug, title, description, seller_id) VALUES(:slug, :title, :description, :seller_id)""")
+select_last_item_id = text("""SELECT * FROM items ORDER BY id DESC LIMIT 1""")
+item_statement = text("""INSERT INTO items(slug, title, description, seller_id) 
+                    VALUES(:slug, :title, :description, :seller_id)
+                    """)
+comment_statement = text("""
+            INSERT INTO comments (body, seller_id, item_id) 
+            VALUES (:body, :seller_id, :item_id)
+            """)
 
 letters = string.ascii_lowercase
 
@@ -32,10 +37,10 @@ def create_user_and_item(con, slug):
 
     con.execute(user_insert_statement, **user)
 
-    result = con.execute(select_last_user_id)
+    user_id_result = con.execute(select_last_user_id)
 
     generated_user_id = None
-    for row in result:
+    for row in user_id_result:
         generated_user_id = row['id']
 
     item = {
@@ -47,6 +52,22 @@ def create_user_and_item(con, slug):
 
     con.execute(item_statement, **item)
 
+    item_id_result = con.execute(select_last_item_id)
+
+    generated_item_id = None
+    for row in item_id_result:
+        generated_item_id = row['id']
+
+    comment = {
+        'body' : ''.join(random.choice(string.printable) for i in range(40)),
+        'seller_id' : generated_user_id,
+        'item_id' : generated_item_id,
+    }
+
+    con.execute(comment_statement, **comment)
+
+
+
 with engine.connect() as con:
-    for i in range(100):
+    for i in range(101,200):
         create_user_and_item(con, "test item " + str(i))
